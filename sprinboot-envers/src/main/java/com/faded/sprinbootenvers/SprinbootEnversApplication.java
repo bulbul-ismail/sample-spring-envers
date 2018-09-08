d8203b534443cef12035dfa560b3cb9227c3c9e9
ismail <bulbull.ismail@gmail.com>
2018-09-08 16:39:26 +0300

    add initializr

---
+++ sprinboot-envers/src/main/java/com/faded/sprinbootenvers/SprinbootEnversApplication.java
@@ -0,0 +1,12 @@
+package com.faded.sprinbootenvers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinbootEnversApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinbootEnversApplication.class, args);
+	}
+}
